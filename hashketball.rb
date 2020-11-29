2d0bce5a23400a9f87218008a2adbd487c9d5491
Kody Samaroo <kody.samaroo@gmail.com>
2020-11-29 22:03:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -130,7 +130,8 @@ end
 
 # Write code here
 def num_points_scored(name)
-  game_hash.each do |team, 
+  game_hash.each do |team, team_data|
+    team_data.each do |x, y|
   #game_hash[:home][:players][:player_name][name]
     binding.pry
     puts 'hi'
