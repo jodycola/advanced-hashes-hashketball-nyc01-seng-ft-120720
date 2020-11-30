56ae740611330659509affbd4819c2dc24fdc4d8
Kody Samaroo <kody.samaroo@gmail.com>
2020-11-30 00:26:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -159,6 +159,13 @@ def team_names
     return team_data[:team_name]
 end
 
+def player_numbers(team)
+  game_hash.values.each do |team_data|
+    if team_data.has_value?(team)
+      return team_data[:players].map { |player| player[:number]}
+    end
+  end
+end
 
 
 
